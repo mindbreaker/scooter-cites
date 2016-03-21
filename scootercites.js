daa13d26f2bc98d57cbf108ae6cd2960e6e411c9
Tom <tczernik.mmt-b2008@fh-salzburg.ac.at>
2016-03-21 15:01:34 +0100

    Init scooter cites

---
+++ scootercites.js
@@ -0,0 +1,55 @@
+var scooterCites = [{
+    'lyric': "How much is the fish?",
+    'song': "How much is the fish"
+
+}, {
+    'lyric': "I am the horseman!",
+    'song': "One – Always hardcore"
+
+}, {
+    'lyric': "Döb döb döb da da döb döb döb!",
+    'song': "Maria I like it loud"
+
+}, {
+    'lyric': "Respect the man in the ice cream van!",
+    'song': "Weekend"
+
+}, {
+    'lyric': "It’s nice to be important, but it’s more important to be nice!",
+    'song': "Move your ass"
+
+}, {
+    'lyric': "It’s not a bird, it’s not a plane. It must be Dave who’s on the train.",
+    'song': "Maria I like it loud"
+
+}, {
+    'lyric': "Don't throw away the banana boxes!",
+    'song': "Mary Got No Lamb"
+
+}, {
+    'lyric': "Let's shuffle in the air, yeah!",
+    'song': "J'adore Hardcore"
+
+}, {
+    'lyric': "Ta, ta, ta, tadata, ta, ta - Ta, ta, ta, tadata, ta, ta, yeah",
+    'song': "Maria I like it loud"
+
+}, {
+    'lyric': "You git, gudda gudda get up with it",
+    'song': "Maria I like it loud"
+
+}, {
+    'lyric': "It's the first page of the second chapter",
+    'song': "How much is the fish"
+
+},
+{
+    'lyric': "A guaranteed emergency. The radical MC H.P.",
+    'song': "Fire"
+
+},
+{
+    'lyric': "This is Scooter!",
+    'song': "Hyper, hyper"
+
+}, ];
